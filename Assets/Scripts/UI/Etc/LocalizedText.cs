a3d1a57139d03bd673dd5b6fae9602ae8f6f4cc5
kmkm0318 <kmkm0318@naver.com>
2026-01-20 20:09:57 +0900

    LocalizedText 수정

--- Assets/Scripts/UI/Etc/LocalizedText.cs
+++ Assets/Scripts/UI/Etc/LocalizedText.cs
@@ -14,29 +14,77 @@ public class LocalizedText : MonoBehaviour
     private TMP_Text _text;
     #endregion
 
-    private void Start()
+    #region 변수
+    private bool _isRegistered = false;
+    #endregion
+
+    private void Awake()
     {
         // 레퍼런스 초기화
         if (_text == null) _text = GetComponent<TMP_Text>();
+    }
 
-        // null 체크
-        if (_text == null) return;
+    private void Start()
+    {
+        // 싱글톤 초기화 오류를 방지하기 위해 Start에서 OnEnable 호출
+        OnEnable();
+    }
 
-        // 언어 변경 이벤트 구독
-        LocalizationManager.Instance.OnLanguageChanged += UpdateLocalizedText;
+    private void OnEnable()
+    {
+        // 이벤트 구독
+        RegisterEvents();
 
-        // 초기 텍스트 설정
+        // 싱글톤이 없으면 패스
+        if (LocalizationManager.Instance == null) return;
+
+        // 텍스트 초기화
         UpdateLocalizedText(LocalizationManager.Instance.CurrentLanguage);
     }
 
     private void OnDestroy()
     {
-        // 언어 변경 이벤트 구독 해제
-        LocalizationManager.Instance.OnLanguageChanged -= UpdateLocalizedText;
+        // 이벤트 구독 해제
+        UnregisterEvents();
     }
+
+    #region 이벤트 구독, 해제
+    private void RegisterEvents()
+    {
+        // 중복 등록 방지
+        if (_isRegistered) return;
+
+        // 싱글톤이 없으면 패스
+        if (LocalizationManager.Instance == null) return;
+
+        // 이벤트 구독
+        LocalizationManager.Instance.OnLanguageChanged += UpdateLocalizedText;
+
+        // 플래그 설정
+        _isRegistered = true;
+    }
+
+    private void UnregisterEvents()
+    {
+        // 등록되지 않았으면 패스
+        if (!_isRegistered) return;
+
+        // 싱글톤이 없으면 패스
+        if (LocalizationManager.Instance == null) return;
+
+        // 이벤트 구독 해제
+        LocalizationManager.Instance.OnLanguageChanged -= UpdateLocalizedText;
+
+        // 플래그 설정
+        _isRegistered = false;
+    }
+    #endregion
 
     private void UpdateLocalizedText(LanguageType languageType)
     {
+        // 싱글톤이 없으면 패스
+        if (LocalizationManager.Instance == null) return;
+
         // 로컬라이즈된 텍스트 가져오기
         string localizedText = LocalizationManager.Instance.GetLocalizedText(_localizationKey);
 
