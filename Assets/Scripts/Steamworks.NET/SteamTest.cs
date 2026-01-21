6baa2f22d6598d212a9fe70fb853bb9e25522542
kmkm0318 <kmkm0318@naver.com>
2026-01-21 13:56:12 +0900

    Steamworks에 Assembly Definition 추가 SteamTest 내부에 #if !DISABLESTEAMWROKS 추가

--- Assets/Scripts/Steamworks.NET/SteamTest.cs
+++ Assets/Scripts/Steamworks.NET/SteamTest.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 /// <summary>
 /// Steamworks.NET을 테스트하기 위한 클래스
+/// SteamManager에서 정의된 목표 플랫폼이 아닐 경우에는 클래스 내부를 컴파일하지 않음
 /// </summary>
 public class SteamTest : MonoBehaviour
 {
+
+#if !DISABLESTEAMWORKS
     private void Start()
     {
         if (SteamManager.Initialized)
@@ -17,4 +20,5 @@ public class SteamTest : MonoBehaviour
             $"스팀 초기화 실패!".LogError(this);
         }
     }
+#endif
 }
