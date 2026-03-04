3d9ca9c41e5c1c440985081a9a1406b983076ce0
kmkm0318 <kmkm0318@naver.com>
2026-03-04 15:55:43 +0900

    결과 창에서 뒤로가기 버튼 클릭 시 확인 창을 닫도록 설정

--- Assets/Scripts/Managers/GameManager/StateMachine/States/GameResultState.cs
+++ Assets/Scripts/Managers/GameManager/StateMachine/States/GameResultState.cs
@@ -1,20 +1,26 @@
+using System;
+
 /// <summary>
 /// 게임 결과 상태
 /// </summary>
 public class GameResultState : GameBaseState
 {
     #region 레퍼런스
+    private InputManager _inputManager;
     private RoundManager _roundManager;
     private UserDataManager _userDataManager;
     private ResultPresenter _resultPresenter;
+    private ConfirmPresenter _confirmPresenter;
     #endregion
 
     public GameResultState(GameManager gameManager, GameStateMachine stateMachine, GameStateFactory factory) : base(gameManager, stateMachine, factory)
     {
         // 레퍼런스 설정
+        _inputManager = InputManager.Instance;
         _roundManager = gameManager.RoundManager;
         _userDataManager = gameManager.UserDataManager;
         _resultPresenter = gameManager.GameUIManager.ResultPresenter;
+        _confirmPresenter = gameManager.GameUIManager.ConfirmPresenter;
     }
 
     public override void OnEnter()
@@ -39,16 +45,44 @@ public class GameResultState : GameBaseState
 
         // BGM 재생
         AudioManager.Instance.PlayBGM(BGMType.Result);
+
+        // 이벤트 구독
+        RegisterEvents();
     }
 
     public override void OnExit()
     {
         // 결과 UI 즉시 숨기기
         _resultPresenter.Hide(0f);
+
+        // 이벤트 구독 해제
+        UnregisterEvents();
     }
 
     public override void OnUpdate()
     {
 
     }
+
+    #region 이벤트 구독, 해제
+    private void RegisterEvents()
+    {
+        // 뒤로가기 입력 구독
+        _inputManager.DefaultInput.Player.Back.performed += HandleOnBackPerformed;
+    }
+
+    private void UnregisterEvents()
+    {
+        // 뒤로가기 입력 구독 해제
+        _inputManager.DefaultInput.Player.Back.performed -= HandleOnBackPerformed;
+    }
+    #endregion
+
+    #region 이벤트 핸들러
+    private void HandleOnBackPerformed(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        // 뒤로가기 입력이 들어오면 확인 UI 종료 시도
+        _confirmPresenter.TryCancel();
+    }
+    #endregion
 }
